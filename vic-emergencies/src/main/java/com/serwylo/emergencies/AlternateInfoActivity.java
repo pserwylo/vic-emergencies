fffed6231d22e246e1303f6d7ad803bf67e4a54b
Peter Serwylo <peter@serwylo.com>
2014-01-23 16:57:47 +1100

    WIP: Adding alternate info screen for when there is no internet connection.

---
+++ vic-emergencies/src/main/java/com/serwylo/emergencies/AlternateInfoActivity.java
@@ -0,0 +1,16 @@
+package com.serwylo.emergencies;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+
+public class AlternateInfoActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alternate_info);
+    }
+
+}
---
+++ vic-emergencies/src/main/java/com/serwylo/emergencies/views/EmergencyPhoneFragment.java
@@ -0,0 +1,22 @@
+package com.serwylo.emergencies.views; is in progress
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EmergencyPhoneFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate( R.layout.fragment_emergency_phone_details, null );
+    }
+
+}
